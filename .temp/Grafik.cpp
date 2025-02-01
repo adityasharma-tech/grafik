840de4c5b9bf05d9c92e763b643f3420748b2f95
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-01 16:35:57 +0530

    Temp code: Refactor configuration classes to use std::string instead of char* and add methods for creating and finding configurations

--- .temp/Grafik.cpp
+++ .temp/Grafik.cpp
@@ -1,31 +1,89 @@
 #include <vector>
+#include <iostream>
 
 class WifiConfig {
     public:
-        char* id;
-        char* ssid;
-        char* password;
-        char* ipAddress;
+        std::string id;
+        std::string ssid;
+        std::string password;
+        std::string ipAddress;
         int port;
 };
 
 class BluetoothConfig {
     public:
-        char* id;
+        std::string id;
 };
 
 class SerialConfig {
     public:
-        char* id;
+        std::string id;
         int baud;
 };
 
 class SupportAppConfig {
     public:
-        char* id;
+        std::string id;
 };
 
 class GrafikConfig {
     private:
         std::vector<WifiConfig> wifiConfigs;
+        std::vector<BluetoothConfig> bluetoothConfigs;
+        std::vector<SerialConfig> serialConfigs;
+        std::vector<SupportAppConfig> supportAppConfigs;
+
+    public:
+        int createWifiConfig(std::string id){
+            WifiConfig* wifiConfig = this->findWifiConfig(&id) 
+        }
+
+        int createBluetoothConfig() {
+
+        }
+
+        int createSerialConfig() {
+
+        }
+
+        int createSupportAppConfig() {
+
+        }
+
+    private:
+        WifiConfig* findWifiConfig(const std::string& id){
+            for (auto& config : wifiConfigs){
+                if (config.id == id){
+                    return &config;
+                }
+            }
+            return nullptr;
+        }
+
+        BluetoothConfig* findBluetoothConfig(const std::string& id){
+            for (auto& config : bluetoothConfigs){
+                if (config.id == id){
+                    return &config;
+                }
+            }
+            return nullptr;
+        }
+
+        SerialConfig* findSerialConfig(const std::string& id){
+            for (auto& config : serialConfigs){
+                if (config.id == id){
+                    return &config;
+                }
+            }
+            return nullptr;
+        }
+
+        SupportAppConfig* findSupportAppConfig(const std::string& id){
+            for (auto& config : supportAppConfigs){
+                if (config.id == id){
+                    return &config;
+                }
+            }
+            return nullptr;
+        }
 };
