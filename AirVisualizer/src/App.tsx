31b3ab563aa45aeaca7019a6038c692b1351f64c
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-13 01:12:39 +0530

    Update routing in App component; set Dashboard as the default route and remove Home component

--- AirVisualizer/src/App.tsx
+++ AirVisualizer/src/App.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './app'
 import Dashboard from './app/dashboard'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/' element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   )
