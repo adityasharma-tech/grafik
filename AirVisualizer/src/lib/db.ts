06f97e49f9dd94c26678d48549a2a8a055f69594
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-02-15 01:21:01 +0530

    Refactor database initialization; streamline object store creation and remove redundant checks

--- AirVisualizer/src/lib/db.ts
+++ AirVisualizer/src/lib/db.ts
@@ -6,23 +6,19 @@ const createIndexesInStores = () => {
     const request = indexedDB.open(DB_NAME, DB_VERSION);
     request.onupgradeneeded = () => {
       const db = request.result;
-      if (!db.objectStoreNames.contains("loggers"))
         db.createObjectStore("loggers", { keyPath: "loggerId" });
-      if (!db.objectStoreNames.contains("plotters"))
         db.createObjectStore("plotters", { keyPath: "plotterId" });
-      if (!db.objectStoreNames.contains("plots")) {
+      
         const store = db.createObjectStore("plots", {
           keyPath: "plotId",
           autoIncrement: true,
         });
         store.createIndex("plotterId", "plotterId");
-      }
       if (!db.objectStoreNames.contains("logs")) {
         const store = db.createObjectStore("logs", { keyPath: "logId", autoIncrement: true });
         store.createIndex("loggerId", "loggerId")
       }
-      if (!db.objectStoreNames.contains("devices"))
-        db.createObjectStore("devices", { keyPath: "deviceId" });
+      db.createObjectStore("devices", { keyPath: "deviceId" });
     };
     request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
